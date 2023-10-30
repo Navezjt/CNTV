bc11dd3d89e89e3ccf07286c9e6c1f938bfec360
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2023-10-30 11:27:22 -0500

    Update canales.js

--- assets/js/canales.js
+++ assets/js/canales.js
@@ -927,7 +927,7 @@ const listaCanales = {
     },
     'rtve': {
         'nombre': 'RTVE Noticias',
-        'm3u8_url': 'https://www.youtube.com/embed/8GmR9H2iH8Q?si=flc8T9xScgW4tz7h',
+        'iframe_url': 'https://www.youtube.com/embed/8GmR9H2iH8Q?si=flc8T9xScgW4tz7h',
         'pais': 'es'
     },
 // BRASIL
