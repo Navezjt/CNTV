75590cd65cb7c9995087a2ef19efff9292faac56
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2024-02-13 18:45:08 -0500

    Update canales.js

--- assets/js/canales.js
+++ assets/js/canales.js
@@ -1634,7 +1634,7 @@ const listaCanales = {
     },
     'ANTV-l': {
         'nombre': 'ANTV 1',
-        'iframe_url': 'https://player.twitch.tv/?channel=antv_digimed&parent=www.example.com',
+        'iframe_url': 'https://player.twitch.tv/?channel=antv_digimed&parent=Navezjt.github.io',
         'fuente': 'https://www.antvklik.com/',
         'pais': 'id'
     },
