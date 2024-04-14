223075c1ea0a3b15dceea2997615efdb16b933fa
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2024-04-14 12:38:46 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -381,7 +381,7 @@ let listaCanales
 async function fetchCargarCanales() {
   try {
     console.log('Probando carga archivo principal con canales');
-    const response = await fetch('https://raw.githubusercontent.com/Alplox/json-teles/main/canales.json');
+    const response = await fetch('https://raw.githubusercontent.com/Navezjt/json-CNTV/main/canales.json');
     listaCanales = await response.json();
   } catch (error) {
     console.error('Error al cargar el archivo principal canales:', error);
